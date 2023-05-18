fbf8d91d639bcaf527a41f0192274a0635f31e69
MackInTech <jerrykmack803@yahoo.com>
2023-05-18 07:15:20 -0400

    Small crud app to understand spring boot REST service

--- 04-spring-boot-rest-crud-apis/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud-apis/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -48,32 +48,5 @@ public class StudentRestController {
     }
 
     // Add an exception handler using @ExceptionHandler annotation
-    @ExceptionHandler // this method is an exception handler,
-    //<StudentErrorResponse> is the type of response body & StudentNotFoundException is the
-    // exception type to handle/catch
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
 
-        // return ResponseEntity
-        // (the error = body of the response, status code of the response)
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add a new exception handler to catch all exceptions being thrown
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        // (the error = body of the response, status code of the response)
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
